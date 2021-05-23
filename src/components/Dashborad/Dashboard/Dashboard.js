b1256b32a16becafd523550dde6c3f838503ebf1
Aktaruzzaman Joti <aktaruzzamanjoti97@gmail.com>
2021-05-24 02:58:30 +0600

    pray done

--- src/components/Dashborad/Dashboard/Dashboard.js
+++ src/components/Dashborad/Dashboard/Dashboard.js
@@ -2,19 +2,18 @@ import React from 'react';
 import { Switch, Route, Link } from "react-router-dom";
 import Login from '../../Login/Login/Login';
 import AddService from '../AddService/AddService';
+import BookingList from '../BookingList/BookingList';
 
 const Dashboard = () => {
+
     return (
         <div>
             <Switch>
                 <Route path="/dashboard">
-                    <AddService />
+                    <BookingList />
                 </Route>
                 <Route path="/addService">
-                    <AddService />
-                </Route>
-                <Route path="/login">
-                    <Login />
+                    <BookingList />
                 </Route>
             </Switch>
         </div>
--- src/components/Dashborad/Sidebar/Sidebar.js
+++ src/components/Dashborad/Sidebar/Sidebar.js
@@ -1,64 +1,95 @@
-import React from 'react';
-import { faCalendar, faFileAlt } from '@fortawesome/free-regular-svg-icons';
-import { faCog, faGripHorizontal, faHome, faSignOutAlt, faUserPlus, faUsers } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Link } from 'react-router-dom';
-import './Sidebar.css';
-import { faAccessibleIcon } from '@fortawesome/free-brands-svg-icons';
+import React, { useContext, useEffect, useState } from "react";
+import { faCalendar, faFileAlt } from "@fortawesome/free-regular-svg-icons";
+import {
+  faCog,
+  faGripHorizontal,
+  faHome,
+  faSignOutAlt,
+  faUserPlus,
+  faUsers,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
+import "./Sidebar.css";
+import { faAccessibleIcon } from "@fortawesome/free-brands-svg-icons";
+import { UserContext } from "../../../App";
 
 const Sidebar = () => {
-    return (
-        <div className="sidebar d-flex flex-column justify-content-between  py-5 px-4" style={{height:"100vh"}}>
-            
-            <ul className="list-unstyled">
-            <li>
-                    <Link className="text-brand text-decoration-none" to="/dashboard">
-                        <FontAwesomeIcon icon={faGripHorizontal} /> <span>Dashboard</span>
-                    </Link>
-                </li>
-                <li>
-                    <Link to="/home" className="text-brand text-decoration-none">
-                        <FontAwesomeIcon icon={faHome} /> <span>Home</span>
-                    </Link>
-                </li>
-                <li>
-                        <Link to="/bookingList" className="text-brand text-decoration-none">
-                            <FontAwesomeIcon icon={faAccessibleIcon} /> <span>Booking List</span>
-                        </Link>
-                </li>
-                 <div>
-                    <li>
-                        <Link to="/allBookings" className="text-brand text-decoration-none">
-                            <FontAwesomeIcon icon={faCalendar} /> <span>All Bookings</span>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/addService" className="text-brand text-decoration-none">
-                            <FontAwesomeIcon icon={faUsers} /> <span>Add Service</span>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/makeAdmin" className="text-brand text-decoration-none">
-                            <FontAwesomeIcon icon={faFileAlt} /> <span>Make Admin</span>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/manageService" className="text-brand text-decoration-none" >
-                            <FontAwesomeIcon icon={faUserPlus} /> <span>Manage Service</span>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link className="text-brand text-decoration-none" to="/settings">
-                            <FontAwesomeIcon icon={faCog} /> <span>Settings</span>
-                        </Link>
-                    </li>
-                </div>
-            </ul>
-            <div>
-                <Link to="/" className="text-brand text-decoration-none"><FontAwesomeIcon icon={faSignOutAlt} /> <span>Logout</span></Link>
-            </div>
-        </div>
-    );
+  const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+  const [isAdmin, setIsAdmin] = useState(false);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/adminList")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        const checkAdmin = data.find(
+          (checkingPerson) => checkingPerson.email === loggedInUser.email
+        );
+        setIsAdmin(checkAdmin);
+      });
+  }, []);
+
+  return (
+    <div
+      className="sidebar d-flex flex-column justify-content-between  py-5 px-4"
+      style={{ height: "100vh" }}
+    >
+      <ul className="list-unstyled">
+        <li>
+          <Link className="text-brand text-decoration-none" to="/dashboard">
+            <FontAwesomeIcon icon={faGripHorizontal} /> <span>Dashboard</span>
+          </Link>
+        </li>
+        <li>
+          <Link to="/home" className="text-brand text-decoration-none">
+            <FontAwesomeIcon icon={faHome} /> <span>Home</span>
+          </Link>
+        </li>
+        <li>
+          <Link to="/bookingList" className="text-brand text-decoration-none">
+            <FontAwesomeIcon icon={faAccessibleIcon} />{" "}
+            <span>Booking List</span>
+          </Link>
+        </li>
+        <li>
+          <Link className="text-brand text-decoration-none" to="/settings">
+            <FontAwesomeIcon icon={faCog} /> <span>Settings</span>
+          </Link>
+        </li>
+        {isAdmin && <div>
+          <li>
+            <Link to="/allBookings" className="text-brand text-decoration-none">
+              <FontAwesomeIcon icon={faCalendar} /> <span>All Bookings</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="/addService" className="text-brand text-decoration-none">
+              <FontAwesomeIcon icon={faUsers} /> <span>Add Service</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="/makeAdmin" className="text-brand text-decoration-none">
+              <FontAwesomeIcon icon={faFileAlt} /> <span>Make Admin</span>
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/manageService"
+              className="text-brand text-decoration-none"
+            >
+              <FontAwesomeIcon icon={faUserPlus} /> <span>Manage Service</span>
+            </Link>
+          </li>
+        </div>}
+      </ul>
+      <div>
+        <Link to="/" className="text-brand text-decoration-none">
+          <FontAwesomeIcon icon={faSignOutAlt} /> <span>Logout</span>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
