e6fc3aacb9debd2fecffa1018a53a78f7f3c598e
Aktaruzzaman Joti <aktaruzzamanjoti97@gmail.com>
2021-05-25 19:38:46 +0600

    shob tw shes e almost

--- src/components/Dashborad/BookingListChild/BookingListChild.js
+++ src/components/Dashborad/BookingListChild/BookingListChild.js
@@ -2,19 +2,18 @@ import React from "react";
 import { Card, ListGroup, ListGroupItem } from "react-bootstrap";
 
 const BookingListChild = (props) => {
-  const { name, email, selectedService, serviceDate, shipmentInfo, status } =
-    props.user;
+  const { selectedService, serviceDate, shipmentInfo, status } = props.user;
 
   return (
     <div className="col-md-4 offset-md-1">
-      <Card className="my-3 box-style" style={{ }}>
-       <div className="d-flex">
-       <Card.Img
-          style={{ height: "150px", width: "200px", padding: "20px" }}
-          variant="top"
-          src={selectedService.imageURL}
-        />
-        <Card.Text className="my-5">
+      <Card className="my-3 box-style" style={{}}>
+        <div className="d-flex">
+          <Card.Img
+            style={{ height: "150px", width: "200px", padding: "20px" }}
+            variant="top"
+            src={selectedService.imageURL}
+          />
+          <Card.Text className="my-5">
             {status === "pending" ? (
               <span
                 style={{
@@ -49,22 +48,23 @@ const BookingListChild = (props) => {
               </span>
             )}
           </Card.Text>
-
-
-       </div>
+        </div>
         <Card.Body>
           <Card.Title>{shipmentInfo.service}</Card.Title>
 
           <Card.Text>
-            Name: <span className="mx-2 text-success">{name}</span>
+            Name: <span className="mx-2 text-success">{shipmentInfo.name}</span>
           </Card.Text>
           <Card.Text>
-            Customer Email: <span className="text-danger"><br />{email}</span>
+            Customer Email:{" "}
+            <span className="text-danger">br{shipmentInfo.email}</span>
           </Card.Text>
           <Card.Text>
-            Servicing Date: <span className="text-info"><br /> {serviceDate}</span>
+            Servicing Date:{" "}
+            <span className="text-info">
+              <br /> {serviceDate}
+            </span>
           </Card.Text>
-          
         </Card.Body>
       </Card>
     </div>
